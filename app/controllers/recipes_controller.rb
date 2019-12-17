129ba737f0ff13a68f032798e65f7e0a70bf21a4
Shubham <shubham@gmail.com>
2019-12-17 13:13:06 +0530

    complete feature of cookboot recipe create, update and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,7 +25,6 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to @recipe, notice: 'Recipe was successfully created.' }
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 class Recipe < ApplicationRecord
 
- validates_length_of :title, :within => 1..20
- validates_uniqueness_of :title, :message => "already exists"
+ validates_length_of :tittle, :within => 1..20
+ validates_uniqueness_of :tittle, :message => "already exists"
+
+ validates_length_of :instructions, :within => 10..50
 
 end
